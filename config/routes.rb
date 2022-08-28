0e9557fde498a60b6bcbc510997e67696214b077
Gordan Šimunić <simunic.gordan@gmail.com>
2022-08-28 16:26:42 +0200

    version api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :books
-  resources :authors
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :books
+      resources :authors
+    end
+  end    
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
